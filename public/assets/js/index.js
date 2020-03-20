c3a3c3b7acc8c5273d4076ad7e7a9b068b6e51bb
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-03-20 16:52:51 -0700

    added logo images

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -134,4 +134,31 @@ $.get("https://api.github.com/repos/AndresF97/monopoly_tracker")
     $(document).ready(function(){
       $('.parallax').parallax();
     });
-       
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+$(document).on("click","#githubBtn",function(event){
+  event.preventDefault()
+  $.get("https://api.github.com/users/"+$("#input_text").val().trim())
+  .then((res)=>{
+    console.log(res)
+    const carousel = $(".carousel");
+    const mainCarousel = $("<div>")
+    mainCarousel.addClass("carousel-item")
+    const carouselItem = $("<div>");
+    carouselItem.addClass("carousel-item")
+    carouselItem.html(`
+    <div class="row valign-wrapper">
+      <div class="col s2">
+        <img src=${res.avatar_url} alt="" class="circle responsive-img">
+      </div>
+      <div class="col s10">
+        <span class="black-text">
+            ${res.bio}
+        </span>
+      </div>
+    </div>
+  `)
+  carousel.append(carouselItem)
+  })
+})  
